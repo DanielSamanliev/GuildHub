79a1d1d4b00917e6ee800fedcfee70fe6d99f984
Daniel Samanliev <dansamanliev@gmail.com>
2020-12-27 00:52:26 +0200

    Migrations for previous commit

---
+++ Data/GuildHub.Data/Migrations/20201226144652_AddAwardedOnDateForTrophies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuildHub.Data.Migrations
+{
+    public partial class AddAwardedOnDateForTrophies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AwardedOn",
+                table: "UsersTrophies",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AwardedOn",
+                table: "GuildsTrophies",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AwardedOn",
+                table: "UsersTrophies");
+
+            migrationBuilder.DropColumn(
+                name: "AwardedOn",
+                table: "GuildsTrophies");
+        }
+    }
+}
